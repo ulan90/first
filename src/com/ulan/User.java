3a685f00b5aa56b2bc68220421ca9eef5b093640
Ulan <Ulan@Ulan-PC>
2015-02-11 12:16:19 +0600

    asd

---
+++ src/com/ulan/User.java
@@ -0,0 +1,29 @@
+package com.ulan;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean
+@SessionScoped
+public class User {
+	private String name="";
+	private String password;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getGreeting() {
+		if (name.length() == 0) return "";
+		else return "Welcome to JSF2 + Ajax, " + name + "! " + password;
+	}
+}
